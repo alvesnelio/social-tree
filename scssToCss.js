aa7e957d25a93d910a461917e8f47c4671a47b75
Nélio Alves <nelio13alves@gmail.com>
2022-10-12 15:07:08 -0400

    feat(Development): :sparkles: Desenvolvimento do defaio social tree da rocket seat

--- scssToCss.js
+++ scssToCss.js
@@ -2,13 +2,21 @@ const sass = require('sass');
 const fs = require('fs');
 const path = require('path');
 
-const styleCompressed = sass.compile('public/scss/style.scss', {style: "compressed"});
+const styleCompressed = sass.compile('public/scss/style.scss');
+// const styleCompressed = sass.compile('public/scss/style.scss', {style: "compressed"});
 
-fs.writeFileSync(
-  path.join(
+console.log("");
+console.log("Converting scss to css...");
+
+function executeConversion(){
+  const result = fs.writeFileSync(
+    path.join(
       __dirname,
       'public/css/style.css'
     ),
     styleCompressed.css.toString()
-  )
-;
+  );
+}
+
+executeConversion();
+//const converion = setInterval(executeConversion, 500);
